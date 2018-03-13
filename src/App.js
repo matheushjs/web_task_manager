e2b62c71a2863db7e7eca599617ff1e418fc9300
Matheus Saldanha <matheus.saldanha@usp.br>
2018-03-12 23:30:52 -0300

    Set basic structure of task views

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <div className="task-vbox">
+          <div className="task-top">One</div>
+          <div className="task-bottom">Two</div>
+        </div>
+        <div className="task-vbox">
+          <div className="task-top">One</div>
+          <div className="task-bottom">Two</div>
+        </div>
+        <div className="task-vbox">
+          <div className="task-top">One</div>
+          <div className="task-bottom">Two</div>
+        </div>
       </div>
     );
   }
