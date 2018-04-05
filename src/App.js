fb36dc07175723cdf1c94729c839f6cbf1d726e8
Matheus Saldanha <matheus.saldanha@usp.br>
2018-04-04 22:19:57 -0300

    Modularize ItemBox

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function ItemBox(props) {
+  return (
+    <div className="itembox-top">
+      <div className="itembox-content">
+        {props.items.map((element, idx) => <p key={idx}>{element}</p>)}
+      </div>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="task-vbox">
-          <div className="task-top">
-            <div className="task-content">
-              Hey<br/>
-              Hey<br/>
-              Hey<br/>
-            </div>
-          </div>
+          <ItemBox items={["Hey", "Hey", "Hey"]} />
           <div className="task-bottom">
             <input className="task-input" type="text" name="fname"/>
           </div>
         </div>
         <div className="task-vbox">
-          <div className="task-top">
-            <div className="task-content">
-              Hey<br/>
-              Hey<br/>
-              Hey<br/>
-            </div>
-          </div>
+          <ItemBox items={["Hey", "Hey", "Hey"]} />
           <div className="task-bottom">
             <input className="task-input" type="text" name="fname"/>
           </div>
         </div>
         <div className="task-vbox">
-          <div className="task-top">
-            <div className="task-content">
-              Hey<br/>
-              Hey<br/>
-              Hey<br/>
-            </div>
-          </div>
+          <ItemBox items={["Hey", "Hey", "Hey"]} />
           <div className="task-bottom">
             <input className="task-input" type="text" name="fname"/>
           </div>
