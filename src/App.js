a2a990b2c65ff58071138b6aee5676dae58e5fa9
Matheus Saldanha <matheus.saldanha@usp.br>
2018-04-04 23:00:27 -0300

    Add input box to ItemBox

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 function ItemBox(props) {
+  // props.items, props.hasInput, props.onSubmit
+  let input = ""
+  if(props.hasInput){
+    input = 
+      <div className="itembox-inputbox">
+        <form onSubmit={props.onSubmit}>
+          <input className="itembox-input" type="text" name="fname"/>
+        </form>
+      </div>
+  }
+
   return (
-    <div className="itembox-top">
-      <div className="itembox-content">
+    <div className="itembox-container">
+      <div className="itembox-items">
         {props.items.map((element, idx) => <p key={idx}>{element}</p>)}
       </div>
+        {input}
     </div>
   );
 }
@@ -15,24 +27,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="task-vbox">
-          <ItemBox items={["Hey", "Hey", "Hey"]} />
-          <div className="task-bottom">
-            <input className="task-input" type="text" name="fname"/>
-          </div>
-        </div>
-        <div className="task-vbox">
-          <ItemBox items={["Hey", "Hey", "Hey"]} />
-          <div className="task-bottom">
-            <input className="task-input" type="text" name="fname"/>
-          </div>
-        </div>
-        <div className="task-vbox">
-          <ItemBox items={["Hey", "Hey", "Hey"]} />
-          <div className="task-bottom">
-            <input className="task-input" type="text" name="fname"/>
-          </div>
-        </div>
+          <ItemBox items={["Hey", "Hey", "Hey"]} hasInput={true} onSubmit={() => alert("hey")} />
+          <ItemBox items={["Hey", "Hey", "Hey"]} hasInput={true} onSubmit={() => alert("hey")}/>
+          <ItemBox items={["Hey", "Hey", "Hey"]} hasInput={true} onSubmit={() => alert("hey")}/>
       </div>
     );
   }
